571fa823fe4c95869df7639846cf75ba1d837692
Gareth <gsveitch@gmail.com>
2018-01-04 21:53:38 -0600

    [client] implement filtered search and rerendered map pinpoints

--- src/components/browse.vue
+++ src/components/browse.vue
@@ -10,7 +10,7 @@
                 <br>
                 <br>
                 PotLuckies
-                <ul id="example-1">
+                <ul>
                     <li v-for="pot in potLuckies">{{pot.Name}}</li>
                 </ul>
             </b-col>
@@ -43,84 +43,88 @@ export default {
             markers: [],
 
             potLuckies: [],
+            query:'',
         }
     },
     methods:{
         search: function(query) {
-            console.log('searching for ', query);
-            console.log('and pushing to potLuckies')
             this.potLuckies.push(query)
             this.$http.get('/browse')
                 .then(response => {
-                    console.log('response.body!!!!!!!!!\n', response.body);
                     let matches = response.body.filter(event => event.Name.toLowerCase().includes(query.toLowerCase()));
                     this.potLuckies = matches;
+                    this.getMapPoints(this.potLuckies);
+                    console.log('matches', matches)
                 })
-        }
+        },
+        getMapPoints: function(events) {
+            this.markerCoordinates = [{
+                latitude: 29.9511,
+                longitude: -90.0715
+            }];
+            this.bounds = new google.maps.LatLngBounds();
+            const element = document.getElementById(this.mapName)
+            const mapCentre = this.markerCoordinates[0]
+            const options = {
+                center: new google.maps.LatLng(mapCentre.latitude, mapCentre.longitude)
+            }
+            this.map = new google.maps.Map(element, options);
+            const context = this;
+            let arr = [];
+            events.forEach(function(element) {
+                let tempLat = element.LocationLat;
+                let tempLong = element.LocationLng;
+                let eventInfo = element;
+                arr.push({ latitude: tempLat, longitude: tempLong, event: eventInfo })
+            });
+            arr.forEach((coord) => {
+                const position = new google.maps.LatLng(coord.latitude, coord.longitude);
+                var contentString = 
+                '<div>' +
+                '<h2>' + `${coord.event.Name}` + '</h2>' +
+                '<p>' + 'Host: ' + `${coord.event.Host}` + '</p>' +
+                '<p>' + 'Address: ' + `${coord.event.Address}` + '</p>' +
+                '<button id="request" onclick="window.clickMe()">Click me</button>' +
+                '</div>'
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+                var marker = new google.maps.Marker({
+                    position,
+                    map: this.map,
+                    event: coord.event,
+                });
+    
+                marker.addListener('click', function() {
+                    infowindow.open(this.map, marker);
+                    let message = document.getElementById('request');
+                    message.addEventListener('click', () => {
+                        context.$http.post('/request', {
+                            name: marker.event.Name,
+                        }).then(function(response) {
+                            socket.emit('request', {
+                                eventName: marker.event.Name,
+                            })
+                            console.log(response);
+                        })
+                    
+                        });
+    
+                });
+                this.markers.push(marker)
+                this.map.fitBounds(this.bounds.extend(position))
+    
+            });
+
+        },
     },
 
     mounted: function() {
         const socket = io.connect();
-        this.bounds = new google.maps.LatLngBounds();
-        const element = document.getElementById(this.mapName)
-        const mapCentre = this.markerCoordinates[0]
-        const options = {
-            center: new google.maps.LatLng(mapCentre.latitude, mapCentre.longitude)
-        }
-        this.map = new google.maps.Map(element, options);
-        const context = this;
-
         this.$http.get('/browse')
             .then(function(response) {
-                let arr = [];
                 this.potLuckies = response.body;
-                response.body.forEach(function(element) {
-                    let tempLat = element.LocationLat;
-                    let tempLong = element.LocationLng;
-                    let eventInfo = element;
-                    arr.push({ latitude: tempLat, longitude: tempLong, event: eventInfo })
-                });
-                arr.forEach((coord) => {
-                    const position = new google.maps.LatLng(coord.latitude, coord.longitude);
-                    var contentString = 
-                    '<div>' +
-                    '<h2>' + `${coord.event.Name}` + '</h2>' +
-                    '<p>' + 'Host: ' + `${coord.event.Host}` + '</p>' +
-                    '<p>' + 'Address: ' + `${coord.event.Address}` + '</p>' +
-                    '<button id="request" onclick="window.clickMe()">Click me</button>' +
-                    '</div>'
-                    var infowindow = new google.maps.InfoWindow({
-                        content: contentString
-                    });
-                    var marker = new google.maps.Marker({
-                        position,
-                        map: this.map,
-                        event: coord.event,
-                    });
-
-                    marker.addListener('click', function() {
-                        infowindow.open(this.map, marker);
-                        let message = document.getElementById('request');
-                        message.addEventListener('click', () => {
-                            context.$http.post('/request', {
-                                name: marker.event.Name,
-                            }).then(function(response) {
-                                socket.emit('request', {
-                                    eventName: marker.event.Name,
-                                })
-                                console.log(response);
-                            })
-                        
-                            });
-
-                    });
-                    this.markers.push(marker)
-                    this.map.fitBounds(this.bounds.extend(position))
-
-                });
-                
-
-
+                this.getMapPoints(this.potLuckies);
             })
 
         }
